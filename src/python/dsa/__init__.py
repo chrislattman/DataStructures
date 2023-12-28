e4e8ea26872d8a31dbea762e45e2d5dfdc110ddc
Chris Lattman <natgeo13@gmail.com>
2023-12-28 14:39:52 -0500

    update

--- src/python/dsa/__init__.py
+++ src/python/dsa/__init__.py
@@ -1,5 +1,6 @@
 # Note: this script is executed when this package (dsa) or any of its submodules is imported
 # __all__ defines what modules get imported when "from dsa import *" is ran
+# Then classes can be used as e.g. myarraylist.MyArrayList
 __all__ = [
     "algorithms",
     "myarraylist",
